5c8b39a9eaa022293a3a94e3b386d87ceda55097
Jerome Brown <jeromebrown@macbookpro.home>
2025-06-28 21:40:59 +0100

    Align posted time right in job card, adjust padding for balanced header row layout

--- src/components/JobCard/JobCard.jsx
+++ src/components/JobCard/JobCard.jsx
@@ -5,8 +5,10 @@ function JobCard() {
   return (
     <Link to="/jobs/1" className="job-card-link">
       <div className="job-card">
-        <div className="posted-time">1 day ago</div>
-        <h2 className="job-title">Senior Web Designer</h2>
+        <div className="job-card-header-row">
+          <h2 className="job-title">Senior Web Designer</h2>
+          <div className="posted-time">1 day ago</div>
+        </div>
         <div className="company">Company: Microsoft</div>
         <div className="tags">
           <span className="tag">Location - Remote</span>
